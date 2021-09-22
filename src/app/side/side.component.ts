3f5517d8baa7235c9ce95e64acf2d459a3a79d08
Gustav Sundin <sundin666@gmail.com>
2021-09-22 14:37:49 +0200

    Improved & cleaner drag-n-drop

--- src/app/side/side.component.ts
+++ src/app/side/side.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Track } from '../track';
+import {CdkDragDrop, CdkDropList, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+
 
 @Component({
   selector: 'app-side',
@@ -18,6 +20,18 @@ export class SideComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onDrop(event: CdkDragDrop<Track[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex, event.currentIndex);
+    }
   }
 
 }
